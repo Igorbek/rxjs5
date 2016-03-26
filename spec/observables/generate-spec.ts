84b7ddb7fe5f9125d2f78e2165dc12d84a0bd653
Igor Oleinikov <igor@oleinikov.ru>
2016-03-25 19:01:59 -0700

    feat(Observable): initially create Observable.generate factory

---
+++ spec/observables/generate-spec.ts
@@ -0,0 +1,60 @@
+import * as Rx from '../../dist/cjs/Rx.KitchenSink';
+import '../../dist/cjs/add/observable/generate';
+import {TestScheduler} from '../../dist/cjs/testing/TestScheduler';
+declare const {expectObservable};
+declare const rxTestScheduler: TestScheduler;
+
+const Observable = Rx.Observable;
+
+describe('Observable.generate', () => {
+  it('should complete if condition does not meet', () => {
+    const source = Observable.generate(1, x => false, x => x + 1);
+    const expected = '|';
+
+    expectObservable(source).toBe(expected);
+  });
+  it('should produce first value immediately', () => {
+    const source = Observable.generate(1, x => x == 1, x => x + 1);
+    const expected = '(1|)';
+
+    expectObservable(source).toBe(expected, { '1': 1 });
+  });
+  it('should produce all values synchronously', () => {
+    const source = Observable.generate(1, x => x < 3, x => x + 1);
+    const expected = '(12|)';
+
+    expectObservable(source).toBe(expected, { '1': 1, '2': 2 });
+  });
+  it('should stop producing when unsubscribed', () => {
+    const source = Observable.generate(1, x => x < 4, x => x + 1);
+    let count = 0;
+    const subscriber = new Rx.Subscriber<number>(
+      x => {
+        count++;
+        if (x == 2) {
+          subscriber.unsubscribe();
+        }
+      }
+    );
+    source.subscribe(subscriber);
+    expect(count).toBe(2);
+  });
+  it('should accept a scheduler', () => {
+    const source = Observable.generate({
+      initialState: 1,
+      condition: x => x < 4,
+      iterate: x => x + 1,
+      resultSelector: x => x,
+      scheduler: rxTestScheduler});
+    const expected = '(123|)';
+
+    let count = 0;
+    source.subscribe(x => count++);
+
+    expect(count).toBe(0);
+    rxTestScheduler.flush();
+    expect(count).toBe(3);
+
+    expectObservable(source).toBe(expected, { '1': 1, '2': 2, '3': 3 });
+  });
+});
---
+++ src/observable/GenerateObservable.ts
@@ -0,0 +1,129 @@
+import {Observable} from '../Observable' ;
+import {Scheduler} from '../Scheduler';
+import {Subscriber} from '../Subscriber';
+import {Subscription} from '../Subscription';
+import {Action} from '../scheduler/Action';
+
+import {isScheduler} from '../util/isScheduler';
+
+const selfSelector = <T>(value: T) => value;
+
+export type ConditionFunc<S> = (state: S) => boolean;
+export type IterateFunc<S> = (state: S) => S;
+export type ResultFunc<S, T> = (state: S) => T;
+
+interface SchedulerState<T, S> {
+  state: S;
+  params: {
+    subscriber: Subscriber<T>;
+    condition?: ConditionFunc<S>;
+    iterate: IterateFunc<S>;
+    resultSelector: ResultFunc<S, T>;
+  };
+}
+
+export interface GenerateOptions<T, S> {
+  initialState: S;
+  condition?: ConditionFunc<S>;
+  iterate: IterateFunc<S>;
+  resultSelector: ResultFunc<S, T>;
+  scheduler?: Scheduler;
+}
+
+export class GenerateObservable<T, S> extends Observable<T> {
+  constructor(
+    private initialState: S,
+    private condition: ConditionFunc<S>,
+    private iterate: IterateFunc<S>,
+    private resultSelector: ResultFunc<S, T>,
+    private scheduler?: Scheduler) {
+      super();
+  }
+
+  static create<T, S>(
+    initialState: S,
+    condition: ConditionFunc<S>,
+    iterate: IterateFunc<S>,
+    resultSelector: ResultFunc<S, T>,
+    scheduler?: Scheduler): Observable<T>
+  static create<S>(
+    initialState: S,
+    condition: ConditionFunc<S>,
+    iterate: IterateFunc<S>,
+    scheduler?: Scheduler): Observable<S>
+  static create<T, S>(
+    options: GenerateOptions<T, S>): Observable<T>
+  static create<T, S>(
+    initialStateOrOptions: S | GenerateOptions<T, S>,
+    condition?: ConditionFunc<S>,
+    iterate?: IterateFunc<S>,
+    resultSelectorOrObservable?: (ResultFunc<S, T>) | Scheduler,
+    scheduler?: Scheduler): Observable<T> {
+    if (arguments.length == 1) {
+      return new GenerateObservable<T, S>(
+        (<GenerateOptions<T, S>>initialStateOrOptions).initialState,
+        (<GenerateOptions<T, S>>initialStateOrOptions).condition,
+        (<GenerateOptions<T, S>>initialStateOrOptions).iterate,
+        (<GenerateOptions<T, S>>initialStateOrOptions).resultSelector || selfSelector,
+        (<GenerateOptions<T, S>>initialStateOrOptions).scheduler);
+    }
+
+    if (resultSelectorOrObservable === undefined || isScheduler(resultSelectorOrObservable)) {
+      return new GenerateObservable<T, S>(
+        <S>initialStateOrOptions,
+        condition,
+        iterate,
+        selfSelector,
+        <Scheduler>resultSelectorOrObservable);
+    }
+
+    return new GenerateObservable<T, S>(
+      <S>initialStateOrOptions,
+      condition,
+      iterate,
+      <ResultFunc<S, T>>resultSelectorOrObservable,
+      <Scheduler>scheduler);
+  }
+
+  protected _subscribe(subscriber: Subscriber<any>): Subscription | Function | void {
+    let state = this.initialState;
+    if (this.scheduler) {
+      return this.scheduler.schedule<SchedulerState<T, S>>(GenerateObservable.dispatch, 0, {
+        params: {
+          subscriber,
+          iterate: this.iterate,
+          condition: this.condition,
+          resultSelector: this.resultSelector
+        }, state });
+    }
+    do {
+      if (!this.condition(state)) {
+        subscriber.complete();
+        break;
+      }
+      let value = this.resultSelector(state);
+      subscriber.next(value);
+      if (subscriber.isUnsubscribed) {
+        break;
+      }
+      state = this.iterate(state);
+    } while (true);
+  }
+  
+  private static dispatch<T, S>(state: SchedulerState<T, S>) {
+    if (state.params.subscriber.isUnsubscribed) {
+      return;
+    }
+    if (!state.params.condition(state.state)) {
+      state.params.subscriber.complete();
+      return;
+    }
+    const value = state.params.resultSelector(state.state);
+    state.params.subscriber.next(value);
+    if (state.params.subscriber.isUnsubscribed) {
+      return;
+    }
+    const nextState = state.params.iterate(state.state);
+    (<Action><any>this).schedule({ state: nextState, params: state.params });
+  }
+}
